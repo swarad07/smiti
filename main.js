d9d3a48b33cc6e80868a2653ab99fcdf65ddbb64
Swarad <swarad07@gmail.com>
2024-03-13 21:18:51 +0530

    Final updates.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-console.log('here');
 const year = new Date().getFullYear();
 const eighteenMarch = new Date(year, 2, 18).getTime();
 const eighteenMarchNextYear = new Date(year + 1, 2, 18).getTime();
 const month = new Date().getMonth();
 
 // countdown
+
+// display
 let timer = setInterval(function() {
 
   // get today's date
@@ -12,7 +13,7 @@ let timer = setInterval(function() {
 
   // get the difference
   let diff;
-  if(month > 6) {
+  if (month > 6) {
     diff = eighteenMarchNextYear - today;
   } else {
     diff = eighteenMarch - today;
@@ -26,14 +27,31 @@ let timer = setInterval(function() {
 
   // display
   document.getElementById("timer").innerHTML =
-    "<div class=\"days\"> \
+    "<div class=\" card days\"> \
   <div class=\"numbers\">" + days + "</div>days</div> \
-<div class=\"hours\"> \
+<div class=\" card hours\"> \
   <div class=\"numbers\">" + hours + "</div>hours</div> \
-<div class=\"minutes\"> \
+<div class=\" card minutes\"> \
   <div class=\"numbers\">" + minutes + "</div>minutes</div> \
-<div class=\"seconds\"> \
+<div class=\" card seconds\"> \
   <div class=\"numbers\">" + seconds + "</div>seconds</div> \
 </div>";
 
 }, 1000);
+
+const target = window.document.getElementsByClassName('hero')[0];
+console.log(target);
+
+const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
+const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 30%, 1);">${letter}</span>`;
+const flickerAndColorText = text => 
+  text
+    .split('')
+    .map(flickerLetter)
+    .map(colorLetter)
+    .join('');
+const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
+
+
+neonGlory(target);
+target.onclick = ({ target }) =>  neonGlory(target);
